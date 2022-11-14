d6ba90ca90229864c73f74e546d8bdf89bcefce5
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-14 19:51:54 +0100

    Add devise rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,7 +40,6 @@ Devise.setup do |config|
     manager.failure_app = TurboFailureApp
   end
 
-
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
