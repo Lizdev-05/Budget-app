588e897974d223f0771d550e8c632e468ade260c
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-14 12:48:29 +0100

    Generate schema

--- db/migrate/20221114113426_add_user_ref_to_entity.rb
+++ db/migrate/20221114113426_add_user_ref_to_entity.rb
@@ -1,5 +1,5 @@
 class AddUserRefToEntity < ActiveRecord::Migration[7.0]
   def change
-    add_reference :entities, :author, null: false, foreign_key: true
+    add_reference :entities, :author, null: false, foreign_key: { to_table: :users }
   end
 end
