c7a3adbbbf426f9bfb49fa9b6eb31ecc30da03af
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-16 10:44:26 +0100

    Generate user ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
-    user_path(current_user) # your path
+     user_path(current_user) # your path
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, { roles: [] }])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, { roles: [] }])
   end
 end
