a6c3b67705cb9e45c1064a989796e1b610c3b9d9
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-16 16:04:26 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   has_many :categories, dependent: :destroy
-   has_many :businesses, dependent: :destroy
-          
+  has_many :categories, dependent: :destroy
+  has_many :businesses, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -15,5 +14,3 @@ class User < ApplicationRecord
     role == requested_role.to_s
   end
 end
-
-
--- test/models/business_test.rb
+++ test/models/business_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BusinessTest < ActiveSupport::TestCase
   # test "the truth" do
