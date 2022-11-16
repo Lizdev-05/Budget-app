903127d68d092ef6abec8f74dbe3cc67f92be31a
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-11-16 21:01:22 +0100

    Complete splash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(_resource)
-    user_path(current_user) # your path
-  end
+  skip_before_action :authenticate_user!, only: :splash_screen, raise: false
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, { roles: [] }])
-end
-
-private
-
-def splash_screen
-  redirect_to splash_screens_index_path unless user_signed_in?
-end
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
+  end
 end
